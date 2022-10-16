4fd876f116dce0acc736d9eb19ced1cbc998fd26
Hugocod <u.medioli@icloud.com>
2022-10-16 11:39:41 +0200

    done

---
+++ index.php
@@ -0,0 +1,154 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+<?php
+
+    $textBlocks = [
+
+        [
+            "title" => [
+
+                "Come state implementando la recente decisione della Corte di giustizia dell'Unione europea (CGUE) relativa al diritto all'oblio?"
+      
+             ],
+      
+             "description" => [
+      
+                "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum illum cum distinctio libero, dignissimos ipsa perspiciatis molestias quibusdam! Vel natus quos ipsa, qui culpa fugiat ab sequi suscipit nesciunt quis mollitia amet consequatur nisi animi quaerat libero harum blanditiis labore fugit! Accusamus sed provident aliquam pariatur, natus exercitationem beatae? Accusantium dolore assumenda
+                tenetur quidem facilis? Alias nisi similique repellat deserunt ipsa.<br><br>
+                
+                Porro quisquam veniam vel qui quidem non placeat vero esse quos debitis fugit dicta numquam earum labore architecto, laudantium eaque voluptate facilis, a fuga est odio? Quae, sequi dolor. Soluta esse quam culpa illo harum quasi deserunt, amet obcaecati facilis! Porro quod cum eos. Sint veniam aperiam est alias at distinctio aliquam!
+                Veritatis, asperiores dolores dolorem libero accusantium, quis, amet inventore veniam consectetur reprehenderit aliquam magnam.<br><br>
+                
+                Fugit nihil sunt accusantium quas, soluta autem impedit at ex adipisci aliquid dolorum natus dolorem magnam deserunt commodi pariatur expedita tempore eligendi qui sapiente nobis fuga incidunt! Facere tempore odit voluptatum nobis sequi accusantium itaque, mollitia aperiam minima nulla,
+                iste blanditiis doloremque! Obcaecati iure harum eveniet, sequi laudantium fugit quae tempora facilis neque ipsam numquam vitae. Impedit maxime quia officiis fugit perspiciatis in labore laboriosam et. Possimus ab vero vitae? Atque, consequatur sit?"      
+            ]
+        ]  
+
+        ,
+
+        [
+            "title" => [
+
+                "Come fa Google a proteggere la mia privacy e a tenere le mie informazioni al sicuro?"
+      
+             ],
+      
+             "description" => [
+      
+                "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum illum cum distinctio libero, dignissimos ipsa perspiciatis molestias quibusdam! Vel natus quos ipsa, qui culpa fugiat ab sequi suscipit nesciunt quis mollitia amet consequatur nisi animi quaerat libero harum blanditiis labore fugit! Accusamus sed provident aliquam pariatur, natus exercitationem beatae? Accusantium dolore assumenda
+                tenetur quidem facilis? Alias nisi similique repellat deserunt ipsa.<br><br>
+                
+                Porro quisquam veniam vel qui quidem non placeat vero esse quos debitis fugit dicta numquam earum labore architecto, laudantium eaque voluptate facilis, a fuga est odio? Quae, sequi dolor. Soluta esse quam culpa illo harum quasi deserunt, amet obcaecati facilis! Porro quod cum eos. Sint veniam aperiam est alias at distinctio aliquam!
+                Veritatis, asperiores dolores dolorem libero accusantium, quis, amet inventore veniam consectetur reprehenderit aliquam magnam.<br><br>
+                
+                Fugit nihil sunt accusantium quas, soluta autem impedit at ex adipisci aliquid dolorum natus dolorem magnam deserunt commodi pariatur expedita tempore eligendi qui sapiente nobis fuga incidunt! Facere tempore odit voluptatum nobis sequi accusantium itaque, mollitia aperiam minima nulla,
+                iste blanditiis doloremque! Obcaecati iure harum eveniet, sequi laudantium fugit quae tempora facilis neque ipsam numquam vitae. Impedit maxime quia officiis fugit perspiciatis in labore laboriosam et. Possimus ab vero vitae? Atque, consequatur sit?"      
+            ]
+        ]  
+
+        ,
+
+        [
+            "title" => [
+
+                "Perché il mio account è associato a un paese?"
+      
+             ],
+      
+            "description" => [
+      
+                "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum illum cum distinctio libero, dignissimos ipsa perspiciatis molestias quibusdam! Vel natus quos ipsa, qui culpa fugiat ab sequi suscipit nesciunt quis mollitia amet consequatur nisi animi quaerat libero harum blanditiis labore fugit! Accusamus sed provident aliquam pariatur, natus exercitationem beatae? Accusantium dolore assumenda
+                tenetur quidem facilis? Alias nisi similique repellat deserunt ipsa.<br><br>
+                
+                Porro quisquam veniam vel qui quidem non placeat vero esse quos debitis fugit dicta numquam earum labore architecto, laudantium eaque voluptate facilis, a fuga est odio? Quae, sequi dolor. Soluta esse quam culpa illo harum quasi deserunt, amet obcaecati facilis! Porro quod cum eos. Sint veniam aperiam est alias at distinctio aliquam!
+                Veritatis, asperiores dolores dolorem libero accusantium, quis, amet inventore veniam consectetur reprehenderit aliquam magnam.<br><br>
+                
+                Fugit nihil sunt accusantium quas, soluta autem impedit at ex adipisci aliquid dolorum natus dolorem magnam deserunt commodi pariatur expedita tempore eligendi qui sapiente nobis fuga incidunt! Facere tempore odit voluptatum nobis sequi accusantium itaque, mollitia aperiam minima nulla,
+                iste blanditiis doloremque! Obcaecati iure harum eveniet, sequi laudantium fugit quae tempora facilis neque ipsam numquam vitae. Impedit maxime quia officiis fugit perspiciatis in labore laboriosam et. Possimus ab vero vitae? Atque, consequatur sit?"      
+            ]
+        ]  
+
+        ,  
+        
+        [
+            "title" => [
+
+                "Come faccio a rimuovere informazioni su di me dai risultati di ricerca di Google?"
+      
+             ],
+      
+            "description" => [
+      
+                "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum illum cum distinctio libero, dignissimos ipsa perspiciatis molestias quibusdam! Vel natus quos ipsa, qui culpa fugiat ab sequi suscipit nesciunt quis mollitia amet consequatur nisi animi quaerat libero harum blanditiis labore fugit! Accusamus sed provident aliquam pariatur, natus exercitationem beatae? Accusantium dolore assumenda
+                tenetur quidem facilis? Alias nisi similique repellat deserunt ipsa.<br><br>
+                
+                Porro quisquam veniam vel qui quidem non placeat vero esse quos debitis fugit dicta numquam earum labore architecto, laudantium eaque voluptate facilis, a fuga est odio? Quae, sequi dolor. Soluta esse quam culpa illo harum quasi deserunt, amet obcaecati facilis! Porro quod cum eos. Sint veniam aperiam est alias at distinctio aliquam!
+                Veritatis, asperiores dolores dolorem libero accusantium, quis, amet inventore veniam consectetur reprehenderit aliquam magnam.<br><br>
+                
+                Fugit nihil sunt accusantium quas, soluta autem impedit at ex adipisci aliquid dolorum natus dolorem magnam deserunt commodi pariatur expedita tempore eligendi qui sapiente nobis fuga incidunt! Facere tempore odit voluptatum nobis sequi accusantium itaque, mollitia aperiam minima nulla,
+                iste blanditiis doloremque! Obcaecati iure harum eveniet, sequi laudantium fugit quae tempora facilis neque ipsam numquam vitae. Impedit maxime quia officiis fugit perspiciatis in labore laboriosam et. Possimus ab vero vitae? Atque, consequatur sit?"      
+            ]
+        ]  
+
+        ,  
+        
+        [
+            "title" => [
+
+                "Quando faccio clic sui risultati della Ricerca Google, le mie chiavi di ricerca vengono inviate ai siti web?"
+      
+             ],
+      
+            "description" => [
+      
+                "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum illum cum distinctio libero, dignissimos ipsa perspiciatis molestias quibusdam! Vel natus quos ipsa, qui culpa fugiat ab sequi suscipit nesciunt quis mollitia amet consequatur nisi animi quaerat libero harum blanditiis labore fugit! Accusamus sed provident aliquam pariatur, natus exercitationem beatae? Accusantium dolore assumenda
+                tenetur quidem facilis? Alias nisi similique repellat deserunt ipsa.<br><br>
+                
+                Porro quisquam veniam vel qui quidem non placeat vero esse quos debitis fugit dicta numquam earum labore architecto, laudantium eaque voluptate facilis, a fuga est odio? Quae, sequi dolor. Soluta esse quam culpa illo harum quasi deserunt, amet obcaecati facilis! Porro quod cum eos. Sint veniam aperiam est alias at distinctio aliquam!
+                Veritatis, asperiores dolores dolorem libero accusantium, quis, amet inventore veniam consectetur reprehenderit aliquam magnam.<br><br>
+                
+                Fugit nihil sunt accusantium quas, soluta autem impedit at ex adipisci aliquid dolorum natus dolorem magnam deserunt commodi pariatur expedita tempore eligendi qui sapiente nobis fuga incidunt! Facere tempore odit voluptatum nobis sequi accusantium itaque, mollitia aperiam minima nulla,
+                iste blanditiis doloremque! Obcaecati iure harum eveniet, sequi laudantium fugit quae tempora facilis neque ipsam numquam vitae. Impedit maxime quia officiis fugit perspiciatis in labore laboriosam et. Possimus ab vero vitae? Atque, consequatur sit?"      
+            ]
+        ] 
+
+    ]
+?>
+ 
+ 
+    <div class="container">
+
+        <?php
+
+            foreach($textBlocks as $block){
+            
+            /* metto i valori in apposite variabili */
+            $text = $block['title'][0]; 
+            $description = $block['description'][0];
+
+            /* innietto nell'html */
+        ?>
+    <!--  ////////////////////////////////////////////////////////////////////// HTML dentro PHP --> 
+            
+            <h1> <?= $text ?> </h1>
+            <p> <?= $description ?> </p>
+
+    <!--  ////////////////////////////////////////////////////////////////////// FINE HTML dentro PHP --> 
+        <?php
+            }
+        ?>
+
+    </div>
+
+</body>
+</html>
+
